066f5a8b4979679d5dae2cd5318f02a74dab940b
JohnataAguiar <johnataaguiar.developer@gmail.com>
2024-08-20 08:22:49 -0300

    change

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,9 +1,10 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
     
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         // Váriaveis
         int accountNumber;
@@ -12,6 +13,7 @@ public class ContaTerminal {
 
         // Iniciando 
         System.out.println("Criando conta báncaria");
+        System.out.println("=========================================================");
 
         // Entradas do Usuário
 
@@ -24,6 +26,7 @@ public class ContaTerminal {
         secondName = scanner.next();
 
         // Numero
+        System.out.println("ATENÇÃO! NÚMERO DA CONTA DEVE CONTER 4 DIGITOS.");
         System.out.print("Numero da conta --> ");
         accountNumber = scanner.nextInt();
 
@@ -37,6 +40,15 @@ public class ContaTerminal {
         System.out.print("Saldo --> ");
         bankBalance = scanner.nextDouble();
 
-        
+        // Retorno ao usuário
+
+        System.out.println("=========================================================");
+        System.out.println("Conta criada!" + " " + firstName);
+        System.out.println("=========================================================");
+        System.out.println("Titular da conta --> " + firstName + " " + secondName);
+        System.out.println("Numero da conta --> " + accountNumber);
+        System.out.println("Agência --> " + accountNumber + "-" + digit);
+        System.out.println("Saldo --> " + bankBalance + "R$");
+        System.out.print("=========================================================");
     }
 }
